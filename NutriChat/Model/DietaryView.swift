49dff34bd40534bfb922256364eaae1c74e1ae99
Mayank Raj <mayraj035@gmail.com>
2024-02-12 14:13:05 +0530

    Minor Fixes

--- NutriChat/Model/DietaryView.swift
+++ NutriChat/Model/DietaryView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct DietaryView: View {
+    @Binding var userPreferences: UserPreferences
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form{
+            
+        }
+        
     }
 }
 
 #Preview {
-    DietaryView()
+    DietaryView(userPreferences: .constant(UserPreferencesMockData.generateMockPreferences()))
 }
